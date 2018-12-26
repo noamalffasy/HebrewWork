fbc64ad3edb7f4629a7bd3ba939f82dea23111ac
Noam Alffasy <noahm1234@outlook.com>
2018-12-26 23:40:52 +0200

    Added metadata to page

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,7 +12,7 @@ class MyDocument extends Document {
   componentWillMount() {
     if (typeof window !== "undefined") {
       let images: HTMLImageElement[] = [];
-      
+
       function preload(...urls: string[]) {
         for (let i = 0; i < urls.length; i++) {
           images[i] = document.createElement("img");
@@ -32,6 +32,14 @@ class MyDocument extends Document {
     return (
       <html>
         <Head>
+          <meta charSet="UTF-8" />
+          <title>עבודה בעברית</title>
+          <meta name="description" content="עבודה בעברית 2018" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+
           <link
             rel="stylesheet"
             href={`${ASSET_URL}/static/css/bootstrap/bootstrap-reboot.min.css`}
